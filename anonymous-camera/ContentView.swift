4b00d13754b447d1567398978ba96d3e0b8f952e
Aaron Abentheuer <aaronabentheuer@users.noreply.github.com>
2020-03-17 22:21:42 +0100

    Initial Commit

---
+++ anonymous-camera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  anonymous-camera
+//
+//  Created by Aaron Abentheuer on 17/03/2020.
+//  Copyright © 2020 Aaron Abentheuer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
