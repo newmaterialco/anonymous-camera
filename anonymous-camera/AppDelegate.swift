9574b69a3a96215f00d2e4d0fcccefb99a2ce1b0
Alisdair Mills <alisdairmills@mac.com>
2020-03-20 11:03:37 +0000

    requested audio and photos permission

--- anonymous-camera/AppDelegate.swift
+++ anonymous-camera/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Anon.requestPhotosAccess { _ in
+            Anon.requestMicrophoneAccess { _ in
+            }
+        }
         return true
     }
 
