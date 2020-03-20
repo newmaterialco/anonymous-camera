cdd12d106dc9469c9c2d661b2a6c790fe92c6734
Alisdair Mills <alisdairmills@mac.com>
2020-03-20 15:39:24 +0000

    materials

--- anonymous-camera/ACNoFilterIconView.swift
+++ anonymous-camera/ACNoFilterIconView.swift
@@ -11,13 +11,43 @@ import SnapKit
 import SceneKit
 import SwifterSwift
 
+extension UIColor {
+    func interpolateRGBColorTo(_ end: UIColor, fraction: CGFloat) -> UIColor? {
+        let f = min(max(0, fraction), 1)
+        guard let c1 = self.cgColor.components, let c2 = end.cgColor.components else { return nil }
+        let r: CGFloat = CGFloat(c1[0] + (c2[0] - c1[0]) * f)
+        let g: CGFloat = CGFloat(c1[1] + (c2[1] - c1[1]) * f)
+        let b: CGFloat = CGFloat(c1[2] + (c2[2] - c1[2]) * f)
+        let a: CGFloat = CGFloat(c1[3] + (c2[3] - c1[3]) * f)
+        return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+}
+
 class ACNoFilterIconHostingController: UIViewController {
     
+    var faceNode: SCNNode?
     let sceneView = SCNView()
     var initialPitch = 0.0
     var initialRoll = 0.0
     var initialYaw = 0.0
     let constraint = 180.0
+    let faceNodes = ["node_a"]
+    let featureNodes = ["node_b", "node_c", "node_d"]
+    
+    func animateFaceColor(faceColor: UIColor, featureColor: UIColor, duration: Double) {
+        if let faceNode = faceNode {
+            for childNode in faceNode.childNodes {
+                if let name = childNode.name, let geometry = childNode.geometry {
+                    if faceNodes.contains(name) {
+                        geometry.firstMaterial?.diffuse.contents = faceColor
+                    }
+                    else if featureNodes.contains(name) {
+                        geometry.firstMaterial?.diffuse.contents = featureColor
+                    }
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +63,8 @@ class ACNoFilterIconHostingController: UIViewController {
             let mainScene = SCNScene()
             mainScene.rootNode.addChildNode(node)
             sceneView.scene = mainScene
+            faceNode = node
+            animateFaceColor(faceColor: .white, featureColor: .black, duration: 1.0)
             
             MotionManager.shared.start { pitch, roll, yaw in
                 if self.initialPitch == 0 && self.initialRoll == 0 && self.initialYaw == 0 {
@@ -47,14 +79,7 @@ class ACNoFilterIconHostingController: UIViewController {
                 diffPitch *= (self.constraint / 360)
                 diffRoll *= (self.constraint / 360)
                 diffYaw *= (self.constraint / 360)
-                /*
-                if diffPitch > self.constraint { diffPitch = self.constraint }
-                else if diffPitch < -self.constraint { diffPitch = -self.constraint }
-                if diffRoll > self.constraint { diffRoll = self.constraint }
-                else if diffRoll < -self.constraint { diffRoll = -self.constraint }
-                if diffYaw > self.constraint { diffYaw = self.constraint }
-                else if diffYaw < -self.constraint { diffYaw = -self.constraint }
-                */
+
                 let matrix = SCNMatrix4Identity
                 let pitchRot = SCNMatrix4Rotate(matrix, diffPitch.degreesToRadians.float, 1, 0, 0)
                 let rollRot = SCNMatrix4Rotate(pitchRot, diffRoll.degreesToRadians.float, 0, 0, 1)
--- anonymous-camera/MotionManager.swift
+++ anonymous-camera/MotionManager.swift
@@ -32,13 +32,10 @@ class MotionManager {
                     let quat = data.attitude.quaternion
                     var pitch = atan2(2 * quat.x * quat.w - 2 * quat.y * quat.z, 1 - 2 * quat.x * quat.x - 2 * quat.z * quat.z).radiansToDegrees
                     pitch -= 90
-                    //if pitch < -10 { pitch = -10 }
-                    //else if pitch > 10 { pitch = 10 }
                     let roll = atan2(2 * quat.y * quat.w - 2 * quat.x * quat.z, 1 - 2 * quat.y * quat.y - 2 * quat.z * quat.z).radiansToDegrees
                     let yaw = asin(2 * quat.x * quat.y + 2 * quat.z * quat.w).radiansToDegrees
                     DispatchQueue.main.async {
                         block(pitch, -roll, yaw)
-                        //block(pitch, roll, -yaw)
                     }
                 }
             }
