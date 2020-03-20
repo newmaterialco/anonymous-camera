87245f340cd98db604e3f831c46772c63423e27f
Alisdair Mills <alisdairmills@mac.com>
2020-03-20 13:07:41 +0000

    3d face

--- anonymous-camera/ACNoFilterIconView.swift
+++ anonymous-camera/ACNoFilterIconView.swift
@@ -7,13 +7,37 @@
 //
 
 import SwiftUI
+import SnapKit
+import SceneKit
+import SwifterSwift
 
 class ACNoFilterIconHostingController: UIViewController {
+    
+    let sceneView = SCNView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .blue
+        self.view.addSubview(sceneView)
+        sceneView.snp.makeConstraints { make in
+            make.size.equalToSuperview()
+        }
+        sceneView.backgroundColor = .white
+        if let facePath = Bundle.main.path(forResource: "face", ofType: "dae"), let scene = try? SCNScene(url: URL(fileURLWithPath: facePath), options: nil), let node = scene.rootNode.childNode(withName: "root", recursively: true) {
+            let mainScene = SCNScene()
+            mainScene.rootNode.addChildNode(node)
+            sceneView.scene = mainScene
+            
+            MotionManager.shared.start { pitch, roll, yaw in
+                let matrix = SCNMatrix4Identity
+                let pitchRot = SCNMatrix4Rotate(matrix, pitch.degreesToRadians.float, 1, 0, 0)
+                let rollRot = SCNMatrix4Rotate(pitchRot, roll.degreesToRadians.float, 0, 0, 1)
+                let yawRot = SCNMatrix4Rotate(rollRot, yaw.degreesToRadians.float, 0, 1, 0)
+                mainScene.rootNode.transform = yawRot
+            }
+            
+            //mainScene.rootNode.eulerAngles = SCNVector3(0, 15.degreesToRadians, 0)
+        }
     }
 }
 
